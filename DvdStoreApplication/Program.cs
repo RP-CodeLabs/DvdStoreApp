1af73ff344551d978bf5c86284ee769461a58fa7
Rajesh Ponnam <rajesh.ponnam@bglgroup.co.uk>
2016-06-02 09:17:01 +0100

    Updated program file to implement DvdServiceImpl class

---
+++ DvdStoreApplication/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DvdStoreApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dvdServiceImpl = new DvdServiceImpl();
+            var summary = dvdServiceImpl.GetDvdSummary("DVD-TG423");
+
+            Console.WriteLine($"Summary : {summary}");
+            Console.ReadLine();
+
+        }
+    }
+}
